dc9718f2378d5389b968e17ca593fd66032e5800
Robert Scott Dionne <robertsdionne@gmail.com>
2013-07-09 21:32:25 -0400

    Fix .gitignore to include bin/data/.

---
+++ source/mapreader.cpp
@@ -0,0 +1,3 @@
+#include "mapreader.h"
+
+
---
+++ source/mapreader.h
@@ -0,0 +1,8 @@
+#ifndef __moneymoney__mapreader__
+#define __moneymoney__mapreader__
+
+#include <fstream>
+
+
+
+#endif /* defined(__moneymoney__mapreader__) */
