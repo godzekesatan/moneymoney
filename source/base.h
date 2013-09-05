625dd72d016502b1623529c71342350e7c22296a
Robert Scott Dionne <robertsdionne@gmail.com>
2013-09-04 20:26:33 -0400

    Update DEFINE_INTERFACE.

--- source/base.h
+++ source/base.h
@@ -1,9 +1,15 @@
 #ifndef MONEYMONEY_BASE_H_
 #define MONEYMONEY_BASE_H_
 
+/**
+ * The DECLARE_INTERFACE macro allows us to save some typing when defining a purely abstract base
+ * class.
+ */
 #define DECLARE_INTERFACE(interface)\
 public:\
   virtual ~interface() {}\
+  interface(const interface &) = delete;\
+  interface &operator =(const interface &) = delete;\
 protected:\
   interface() {}\
 private:
